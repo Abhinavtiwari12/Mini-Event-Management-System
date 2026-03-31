11de69f5d34264ba512388e8c720ee4c4f429572
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-03-31 23:43:15 +0530

    fix error

--- src/router/router.js
+++ src/router/router.js
@@ -1,31 +1,24 @@
 import express from 'express';
-
-
 import { registerAdmin, 
     loginAdmin, 
     logoutAdmin, 
     createEvent, 
     updateEvent 
 } from '../controllers/admin.controller.js';
-
 import { registerUser, 
     loginUser, 
     getProfile, 
     logoutUser 
 } from '../controllers/user.controller.js';
-
 import { getEvents } from '../controllers/event.controller.js';
-
 import { bookTicket } from '../controllers/booking.controller.js';
-
 import { recordAttendance } from '../controllers/attendance.controller.js';
-
 import { verifyToken, verifyAdmin } from '../middleware/autho.middleware.js';
 
 const router = express.Router();
 
 
-// --- Admin Routes ---
+// Admin Routes
 router.post('/admin/register', registerAdmin);
 router.post('/admin/login', loginAdmin);
 router.post('/admin/logout', verifyToken, verifyAdmin, logoutAdmin);
@@ -34,20 +27,20 @@ router.put('/admin/events/:id', verifyToken, verifyAdmin, updateEvent);
 
 
 
-// --- Auth / User Routes ---
+// Auth / User Routes
 router.post('/users/register', registerUser);
 router.post('/users/login', loginUser);
 router.get('/users/profile', verifyToken, getProfile);
 router.post('/users/logout', verifyToken, logoutUser);
 
 
-// --- Public Event Routes ---
+// Public Event Routes
 router.get('/events', getEvents);
 
-// --- Booking Routes ---
+// Booking Routes
 router.post('/bookings', verifyToken, bookTicket);
 
-// --- Attendance Routes ---
+// Attendance Routes
 router.post('/attendance', verifyToken, recordAttendance);
 
 export default router;
